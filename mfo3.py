2f78f507177a4f5c3fdb4477857c51a4d8a78e70
krgar99 <127091614+krgar99@users.noreply.github.com>
2023-06-10 23:36:39 +0200

    Update mfo3.py

--- mfo3.py
+++ mfo3.py
@@ -7,19 +7,19 @@ timeout = 260
 timeout_start = time.time()
 left_arrow = 'left'
 right_arrow = 'right'
-heal = 'grave'
-while time.time() < timeout_start + timeout:
+heal_button = 'grave'
 
+while time.time() < timeout_start + timeout:
     keyboard.press(left_arrow)
     time.sleep(long)
-    keyboard.press_and_release(heal)
+    keyboard.press_and_release(heal_button)
     keyboard.release(left_arrow)
     time.sleep(short)
 
-    keyboard.press_and_release(heal)
+    keyboard.press_and_release(heal_button)
 
     keyboard.press(right_arrow)
     time.sleep(long)
     keyboard.release(right_arrow)
 
-    keyboard.press_and_release(heal)
+    keyboard.press_and_release(heal_button)
